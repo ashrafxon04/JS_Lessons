bf301d129a4072276c0ed4a05c3d60f57c13d71f
ashrafxon04 <ashrafyaxyoyev@gmail.com>
2023-06-09 16:20:17 +0500

    Js Veraibles || Data Types

---
+++ Lesson1/hello.js
@@ -0,0 +1,101 @@
+//HomeTask
+
+// let user = {
+//     "id": 1,
+//     "name": "Leanna Gahana",
+//     "username": "Breat",
+//     "email": "gmail.gamail@gmail.com",
+//     "addres": {
+//         "street": "Kulas Light",
+//         "suite": "APT. 556",
+//         "city": "GvenBorught",
+//         "zipcode" : "85889393-48883",
+//         "geo": {
+//             "let" : "-37.9393",
+//             "ling" : "81.92939"
+//         }
+//     },
+//     "phone": "1-770-736-8089  x56442",
+//     "website": "hildgard.org",
+//     "company" : {
+//         "name": "Romaguera-Crona",
+//         "catchPhrase": "Multi-Layerd clind-server neurl-net",
+//         "bs": "harnes real-time e-markets"
+//     }
+// }
+// console.log(`${user.id}
+// user info :
+// ${user.name}
+// ${user.username}
+// ${user.addres.street}
+// ${user.addres.geo.ling}`);
+
+
+
+
+// primitive : number ,{ string , bigint , booleon , null , undefind , symbols}
+
+{//number data type
+    
+// let num = 10
+// let num2 = 20
+
+// console.log( num + num2);
+}
+
+{ //string data type
+
+    // let  str = "Holmat" 
+    // let str2  = 'holmat'
+    // let str3 = `kamol`
+    // console.log( typeof`${str} ${str2} ${str3}`);
+}
+
+{//big int data type
+    // console.log(983784782938239929n + 29n);
+}
+
+{//booleon data type
+    // let i = Boolean()
+    // let k = Boolean()
+    // console.log(i);
+}
+
+{//null data type 
+    // console.log(null + 30);
+}
+
+{// undefind data type 
+    // console.log(undefined + 5);
+    
+}
+
+{//symbols data type
+    // let r = Symbol()
+    // let t = Symbol()
+
+    // console.log(r == t);
+}
+
+// none- primitive : object ( arrey , function , object)
+
+{//object data type
+
+    // let arr = {
+    //     name: "aziz",
+    //     nick: "azaaa",
+    // }
+
+    // let arr1 = {
+    //     name: "aziz",
+    //     nick: "azaaa",
+    // }
+
+    // console.log( arr == arr1);
+
+    //  function getPrice () {
+    //     return 20 + 20
+    // }
+    
+    // console.log(getPrice());
+}
