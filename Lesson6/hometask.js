faa76ef7ec42392815cbfe7266cd1670dae21e36
ashrafxon04 <ashrafyaxyoyev@gmail.com>
2023-06-21 16:10:39 +0500

    Object 1/2 || Basic

--- Lesson6/hometask.js
+++ Lesson6/hometask.js
@@ -1,4 +1,5 @@
-//task1
+//! Task 1 =>
+
 // let rectangle = {
 // 	width: 15,
 // 	height: 25,
@@ -12,13 +13,39 @@
 // console.log( "Yuzi:", rectangle.getArea(rectangle.width, rectangle.height));
 // console.log( "Perimetr:",rectangle.getPerimeter(rectangle.width, rectangle.height));
 
+//! Task 3 =>
 
-//task2
-let uchburchak = {
-    length : 12,
-    height : 5,
-    getArea : function ( a , h ) {
-        return 1/2*a*h;
-    }
-}
-console.log( "Area:",uchburchak.getArea(uchburchak.length,uchburchak.height));
+
+// const circle = {
+// 	radius: 5,
+// 	getArea(r) {
+// 		return 3.14 * r ** 2;
+// 	},
+// 	getCircumference(r) {
+// 		return 2 * 3.14 * r
+// 	}
+// }
+// console.log(circle.getArea(circle.radius));
+// console.log(circle.getCircumference(circle.radius));
+
+// const circle = {
+// 	radius: 10,
+// 	getArea() {
+// 		return 3.14 * this.radius ** 2;
+// 	},
+// 	getCircumference() {
+// 		return 2 * 3.14 * this.radius
+// 	}
+// }
+// console.log(circle.getArea());
+// console.log(circle.getCircumference());
+
+//! Task 3 =>
+// let uchburchak = {
+//     length : 12,
+//     height : 5,
+//     getArea (a, h){
+//         return 1/2*a*h;
+//     }
+// }
+// console.log( "Area:",uchburchak.getArea(uchburchak.length,uchburchak.height));
