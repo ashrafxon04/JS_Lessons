dc07d430737748cd21471218c623b9fcaa916699
ashrafxon04 <ashrafyaxyoyev@gmail.com>
2023-06-21 12:19:33 +0500

    Object 1/2 || Basic

---
+++ Lesson6/hometask.js
@@ -0,0 +1,24 @@
+//task1
+// let rectangle = {
+// 	width: 15,
+// 	height: 25,
+// 	getArea: function(first, second) {
+// 		return first * second;
+// 	},
+//     getPerimeter: function(a, b) {
+//         return 2*(a+b);
+//     }
+// }
+// console.log( "Yuzi:", rectangle.getArea(rectangle.width, rectangle.height));
+// console.log( "Perimetr:",rectangle.getPerimeter(rectangle.width, rectangle.height));
+
+
+//task2
+let uchburchak = {
+    length : 12,
+    height : 5,
+    getArea : function ( a , h ) {
+        return 1/2*a*h;
+    }
+}
+console.log( "Area:",uchburchak.getArea(uchburchak.length,uchburchak.height));
